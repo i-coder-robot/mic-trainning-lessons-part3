14a5d97ea41cea705ce34adb959092e64815ba39
i-coder-robot <63966617@qq.com>
2022-02-20 16:21:48 +0800

    StockNotFound

--- custom_error/custom_error.go
+++ custom_error/custom_error.go
@@ -1,7 +1,7 @@
 package custom_error
 
 const (
-	ProductNotFound = "产品不存在"
-	StockNotEnough  = "库存不足"
-	ParamError      = "参数错误"
+	StockNotFound  = "库存不存在"
+	StockNotEnough = "库存不足"
+	ParamError     = "参数错误"
 )
